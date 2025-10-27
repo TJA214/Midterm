a19d6634828353b5a047ab03fa44549844298d1d
TJA214 <agustint@sheridancollege.ca>
2025-10-27 17:55:17 -0400

    Refactor gradeDetail method to use enum for grades

--- src/printgrades/Grades.java
+++ src/printgrades/Grades.java
@@ -6,26 +6,30 @@ package printgrades;
 
 /**
  *
- * @author sivagamasrinivasan
+ * @author thomasAgustin
  */
 public class Grades {
-  public void gradeDetail(String code)
-{
-  switch(code.toUpperCase())
-        {
-        case "A+": System.out.println("EXCELLENT");
-        break;
-        case "A": System.out.println("VERY GOOD");
-        break;
-        case "B":
-        System.out.println("GOOD");
-        break;
-        case "C": 
-            System.out.println("SATISFACTORY");
-        break;
-        default:
-         System.out.println("NOT VALID CODE");
-        break;
+    public enum Grade {
+        A_PLUS, A, B, C, INVALID
+    }
+
+    public void gradeDetail(Grade code) {
+        switch (code) {
+            case A_PLUS:
+                System.out.println("EXCELLENT");
+                break;
+            case A:
+                System.out.println("VERY GOOD");
+                break;
+            case B:
+                System.out.println("GOOD");
+                break;
+            case C:
+                System.out.println("SATISFACTORY");
+                break;
+            default:
+                System.out.println("NOT VALID CODE");
+                break;
         }
-}
+    }
 }
